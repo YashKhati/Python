429d2e128eec532c37fd6cb558d5caeae60623d9
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2023-08-10 20:13:13 +0530

    Create MergeSort.py

---
+++ Basic Python/Sorting/MergeSort.py
@@ -0,0 +1,42 @@
+def MergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        MergeSort(L)
+        MergeSort(R)
+
+        i=j=k=0
+
+        while i < len(L) and j<len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+arr = []
+n = int(input("Enter size of list : "))
+
+print("Enter List Elements  : ")
+for i in range (0,n):
+    arr.append(int(input()))    
+
+MergeSort(arr)
+
+
+print("Sorted Array : ")
+print(arr)
